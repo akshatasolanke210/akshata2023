45e9472ef1b50373564387bf21289d827bbb9753
akshata <akshatasolanke210@gmail.com>
2023-02-22 10:47:15 +0530

    Commiting the Jenkins Code file

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,6 +3,8 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
       System.out.println("Hello World jenkins CMD Maven");
+      System.out.println("Hello Jenkins this is  my first edit");
+
 	}
 
 }
